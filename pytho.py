f5173036bd2791b40fd4839049cb344130e320ca
joshnareddy202-cyber <joshnareddy202@gmail.com>
2026-03-14 12:02:19 +0000

    datatypes

---
+++ pytho.py
@@ -0,0 +1,6 @@
+# This script prints all even numbers from 1 to 10
+print("Finding even numbers:")
+
+for number in range(1, 11):
+    if number % 2 == 0:
+        print(f"{number} is an even number")
